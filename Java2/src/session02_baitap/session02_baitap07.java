ec2fa0da490cbac3cedf552612bf10b2d859336f
tompetrel <huutaimarvel@gmail.com>
2020-04-04 20:53:43 +0700

    new update

---
+++ Java2/src/session02_baitap/session02_baitap07.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session02_baitap;
+
+/**
+ *
+ * @author TAI
+ */
+public class session02_baitap07 {
+    public static void main(String[] args) {
+        String str1 = "Hello World, Hello javadev";
+        String str2 = "";
+        String regex = " ";
+        System.out.println(str1.replaceAll(regex, str2));
+    }
+}
